7711f56206a7d0a1fe7f424b71237457daffcd29
Suhyeon Lee <85675813+suhyeonnnnn@users.noreply.github.com>
2026-03-12 01:57:48 +0900

    fix: exclude puppeteer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {},
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'puppeteer'];
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
